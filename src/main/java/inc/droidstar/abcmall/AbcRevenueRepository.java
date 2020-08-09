cae0cc36d4a19381c0c072137e19c036a805f5d0
Paras Jain <paras.2426@gmail.com>
2020-08-09 23:28:35 +0530

    Update AbcRevenueRepository.java

--- src/main/java/inc/droidstar/abcmall/AbcRevenueRepository.java
+++ src/main/java/inc/droidstar/abcmall/AbcRevenueRepository.java
@@ -4,4 +4,9 @@ import inc.droidstar.abcmall.AbcRevenue;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AbcRevenueRepository extends CrudRepository<AbcRevenue,Long>{
+    @Query("SELECT Id,Bfrom,Bto,Revenue FROM AbcRevenue WHERE Bfrom>=:Bfrom AND Bto<=:Bto")
+    Iterable<AbcRevenue> getRev(@Param("Bfrom")String Bfrom,@Param("Bto")String Bto);
+
+    @Query("SELECT SUM(Revenue) FROM AbcRevenue WHERE Bfrom>=:Bfrom AND Bto<=:Bto")
+    Integer totRev(@Param("Bfrom")String Bfrom,@Param("Bto")String Bto);
 }
